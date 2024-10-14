5d9de8816c798bee26bb6fd373f8430dad923af2
Austin <austin.mcclain859@myci.csuci.edu>
2024-10-14 13:57:50 -0700

    commit test 3 working script, adds string

--- db/alembic/versions/66d61b7d9162_add_listings.py
+++ db/alembic/versions/66d61b7d9162_add_listings.py
@@ -14,13 +14,13 @@ from datetime import datetime
 
 # revision identifiers, used by Alembic.
 revision: str = '66d61b7d9162'
-down_revision: Union[str, None] = None
+down_revision: Union[str, None] = '81aeb18123d4'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
 
 def upgrade() -> None:
-    op.create_table('users',
+    op.create_table('listings',
                     sa.Column('id', sa.Integer(), autoincrement=True, nullable=False),
 
                     sa.Column('name', sa.String(), nullable=False),
@@ -38,21 +38,18 @@ def upgrade() -> None:
 
                     sa.Column('endTime', sa.DateTime(), autoincrement=False, nullable=False),
 
-                    #for tags we could make a column for each tag, or if we allow users to input custom tags, we would
-                    #likely need to add in some more versatile way of storing tags
-                    sa.Column('electronics', sa.Boolean(), nullable=False),
-                    sa.Column('furniture', sa.Boolean(), nullable=False),
-                    sa.Column('clothing', sa.Boolean(), nullable=False),
+                    #storade as a comma separated list
+                    sa.Column('tags', sa.String(), nullable=True),
 
                     #this version of both price range and rating is based on the assumption that we will be using
                     #a simplified 1-5 dollar sign rating or 1-5 star rating system
-                    sa.Column('priceRange', sa.Integer(), autoincrememnt=False, nullable=False),
+                    sa.Column('priceRange', sa.Integer(), nullable=True),
 
-                    sa.Column('rating', sa.Integer(), autoincrement=False, nullable=False),
+                    sa.Column('rating', sa.Integer(), nullable=True),
 
                     #this version of reviews allows for one singular review, will look into how store multiple per
                     #column, likely some kinda of array of strings
-                    sa.Columm('reviews', sa.String(), nullable=False),
+                    sa.Column('reviews', sa.String(), nullable=True),
 
 
                     sa.PrimaryKeyConstraint('id')
@@ -60,4 +57,4 @@ def upgrade() -> None:
 
 
 def downgrade() -> None:
-    op.drop_table('users')
+    op.drop_table('listings')
